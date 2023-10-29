7c21f3598a082280af96d83d1548c64c6a46a4a9
rafi-IT-tech <65241406+rafi-IT-tech@users.noreply.github.com>
2023-10-30 00:11:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// vite.config.js
-export default {
-  base: '/To-Do-App/',
-};
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/To-Do-App/',
+  plugins: [react()]
 })
